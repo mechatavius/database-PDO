bed16e069086f6adc4e576d5e3e7b006743e2b3d
mechatavius <133094248+mechatavius@users.noreply.github.com>
2023-11-17 13:30:18 +0100

    Add files via upload

--- huiswerk/verwerking.php
+++ huiswerk/verwerking.php
@@ -2,6 +2,6 @@
 //de informatie van het formulier op index.html komt terecht inde variable $_post
 // $_POST is een array;
 // en die kunnen we uitprinten met de functie print_r();
-print_r($_GET);
+print_r($_POST);
 
 //echo "$_POST";
