af0c997fa3dfed1ab01843a654bc2af876565f3d
mechatavius <133094248+mechatavius@users.noreply.github.com>
2023-11-17 13:31:45 +0100

    Delete huiswerk directory

--- huiswerk/verwerking.php
+++
@@ -1,7 +0,0 @@
-<?php
-//de informatie van het formulier op index.html komt terecht inde variable $_post
-// $_POST is een array;
-// en die kunnen we uitprinten met de functie print_r();
-print_r($_POST);
-
-//echo "$_POST";
