c3640679713a0d30ec6698074e5277d604a9ff60
mechatavius <133094248+mechatavius@users.noreply.github.com>
2023-11-17 13:36:06 +0100

    Add files via upload

---
+++ huiswerk/get.php
@@ -0,0 +1,7 @@
+<?php
+//de informatie van het formulier op index.html komt terecht inde variable $_post
+// $_POST is een array;
+// en die kunnen we uitprinten met de functie print_r();
+print_r($_GET);
+
+//echo "$_GET";
